09955dce8cdce2380fac49774f2d398349a9d576
Andres <andresvesso@gmail.com>
2015-09-09 12:48:34 +0300

    Profiili info [Devices #102611636]

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <!doctype html>
-<html lang="en">
+<html lang="et">
 <head>
     <meta charset="UTF-8">
     <title>Valdurist</title>
 </head>
 <body>
 
-    <h3>Valdur Kana</h3>
-    <p>asdfp- depl</p>
+<h3>Valdur Kana</h3>
+<p>tutvustav leht</p>
 
-    <p>Valdur</p>
+<ul>
+    <li>Valdur</li>
+    <li>September 19</li>
+    <li>20 aastat vana</li>
+    <li>Male</li>
+    <li>Tartu, Estonia</li>
+</ul>
+<p>Valdur on noor IT'st ja tehnikast huvitatud mees. Aastal 2011/2012 osales ta noorteprogrammis Entrum, kus ta kuulus projekti nr.13 - "IT-Vabakad".
+    Projektimeeskond toetab Estobuntu e. eestikeelse Linuxi arendamist. Meeskond pidi looma skripti, mis pakuks linuxi kasutajatele lühemaid
+    lahendusi esmatarberakenduste paigaldamiseks. Valdur pälvis sellel konkursil isikliku arengu eripreemia. Entrum tunnustas teda sihikindluse ja
+    suure arengu eest. Tema töö oma oskuste arendamisel oli eriti tähelepanuväärne ja kiiduväärt. Kogu programm sai tal üksi läbi tehtud ning
+    mitmel korral vahetas ta meeskonnaliikmeid, kuid jätkas samal ajal oma projektiidee teostamist.</p>
 
-    <p>About me:</p>
-    <ul>
-        <li>Valdur</li>
-        <li>September 19</li>
-        <li>20 years old</li>
-        <li>Male</li>
-        <li>Tartu, Estonia</li>
-    </ul>
-    <p> Pri in feugiat ancillae phaedrum. Te doctus atomorum ocurreret vis, eam an quaeque forensibus. Sea noster partiendo ne, ad modus meliore vel. Ad eros gubergren vel, mei et ullum graece virtute. In modus tractatos eam, an iusto percipit sed, at sea reque diceret assentior.
-        Nam te fabulas petentium, sea ex causae adolescens. Mei singulis intellegam ne, cu aperiri volumus est. Pro eu virtute eruditi omittantur, duo animal democritum te, ad dicant graeci accumsan vis. Vide propriae scribentur cu mea, appareat vituperata percipitur ut mei. Pro stet dolor consequat ad. Te liber nihil fastidii vim, ei vidit partiendo sed. Corpora eligendi scribentur vis ad, per no ponderum molestiae assueverit.
-        Ut qui sale praesent, vidisse denique vel cu. Ex nostro discere reprehendunt his, mucius ceteros mandamus nam eu, at sea nobis latine recteque. Pro ei graecis fastidii albucius. At adhuc albucius omittantur per, est id suavitate ocurreret. Eius graece dolores ius in, sit ei iracundia quaerendum. Fuisset abhorreant argumentum pri in.
-        His quod oblique ne, pri ea oblique propriae. Eos ut officiis posidonium, natum invidunt voluptatum qui ut, ad nostrud pertinacia vis. Elit iriure periculis sea te. Id vis delenit placerat intellegebat. No falli numquam eam, ex tota mediocrem instructior per, iusto iisque instructior eu pro. Quando explicari vulputate no eos, est illud ullum ad.</p>
 
-    <a href="mailto:valdur55@gmail.com">Write me here</a>
+<a href="mailto:valdur55@gmail.com">Võtke ühendust!</a>
 </body>
 </html>
