1233c8ac6a2b5179da5c28fcb2ef75fbfdc0360c
Andres <andresvesso@gmail.com>
2015-09-09 13:29:09 +0300

    Pilt [Devices #102611538]

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     <li>Male</li>
     <li>Tartu, Estonia</li>
 </ul>
+    <img src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/526749_376307962415416_9973869_n.jpg?oh=5b4ca3137bf8295c86dfab0c8249dda9&oe=566B70F6" />
 <p>Valdur on noor IT'st ja tehnikast huvitatud mees. Aastal 2011/2012 osales ta noorteprogrammis Entrum, kus ta kuulus projekti nr.13 - "IT-Vabakad".
     Projektimeeskond toetab Estobuntu e. eestikeelse Linuxi arendamist. Meeskond pidi looma skripti, mis pakuks linuxi kasutajatele lühemaid
     lahendusi esmatarberakenduste paigaldamiseks. Valdur pälvis sellel konkursil isikliku arengu eripreemia. Entrum tunnustas teda sihikindluse ja
