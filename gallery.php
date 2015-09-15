7e73edd86233811b01d39c489a3d9e4c3062c87d
Andbyu <andresvesso@gmail.com>
2015-09-16 00:42:06 +0300

    Lisatud kirjeldused piltide alla. Lisatud lemmikraamatute nimekiri. Lisatud typeracer'i profiil. Lisatud koolide nimekirja ka haridus. [Delivers #102611636]

--- gallery.php
+++ gallery.php
@@ -8,7 +8,7 @@
 <body>
 <div id="header">
     <h2>Valdur Kana</h2>
-    <h4>Gallerii</h4>
+    <h4>Galerii</h4>
 </div>
 <div class="top">
     <a href="index.php">Pealeht</a>&nbsp;&nbsp;
@@ -17,15 +17,35 @@
 <div class="rightg">
     <table>
         <tr>
-            <td align="center"><a target="_blank" href="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/538789_371930046186541_1805464377_n.jpg?oh=533550b6d0fe2c004ba3b0e31533ec6c&oe=56A1BCE3"><img src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/538789_371930046186541_1805464377_n.jpg?oh=533550b6d0fe2c004ba3b0e31533ec6c&oe=56A1BCE3" alt="Valdur" ;></a>
+            <td align="center"><a target="_blank"
+                                  href="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/538789_371930046186541_1805464377_n.jpg?oh=533550b6d0fe2c004ba3b0e31533ec6c&oe=56A1BCE3"><img
+                        src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/538789_371930046186541_1805464377_n.jpg?oh=533550b6d0fe2c004ba3b0e31533ec6c&oe=56A1BCE3"
+                        alt="Valdur" ;></a>
+
+                <div class="d">ENTRUM auhinnagaala</div>
             </td>
-            <td align="center"><a target="_blank" href="https://scontent-ams2-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/540613_10150608516372142_1096497041_n.jpg?oh=85617ced91b8c0b71ff5ea5c79a8c89e&oe=566A0BE8"><img src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/540613_10150608516372142_1096497041_n.jpg?oh=85617ced91b8c0b71ff5ea5c79a8c89e&oe=566A0BE8" alt="Valdur" ;></a>
+            <td align="center"><a target="_blank"
+                                  href="https://scontent-ams2-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/540613_10150608516372142_1096497041_n.jpg?oh=85617ced91b8c0b71ff5ea5c79a8c89e&oe=566A0BE8"><img
+                        src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/540613_10150608516372142_1096497041_n.jpg?oh=85617ced91b8c0b71ff5ea5c79a8c89e&oe=566A0BE8"
+                        alt="Valdur" ;></a>
+
+                <div class="d">Garage 48</div>
             </td>
         </tr>
         <tr>
-            <td align="center"><a target="_blank" href="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/423363_337245862988293_1857935392_n.jpg?oh=733a36d96e6989cbe1c558ece9fb8983&oe=566D6A01"><img src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/423363_337245862988293_1857935392_n.jpg?oh=733a36d96e6989cbe1c558ece9fb8983&oe=566D6A01" alt="Valdur" ;></a>
+            <td align="center"><a target="_blank"
+                                  href="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/423363_337245862988293_1857935392_n.jpg?oh=733a36d96e6989cbe1c558ece9fb8983&oe=566D6A01"><img
+                        src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/423363_337245862988293_1857935392_n.jpg?oh=733a36d96e6989cbe1c558ece9fb8983&oe=566D6A01"
+                        alt="Valdur" ;></a>
+
+                <div class="d">ENTRUM sessioon</div>
             </td>
-            <td align="center"><a target="_blank" href="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/390211_298268603552686_1393108024_n.jpg?oh=9a39d1f6d0fedea8eccca435b2e7b9ad&oe=56707E76"><img src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/390211_298268603552686_1393108024_n.jpg?oh=9a39d1f6d0fedea8eccca435b2e7b9ad&oe=56707E76" alt="Valdur" ;></a>
+            <td align="center"><a target="_blank"
+                                  href="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/390211_298268603552686_1393108024_n.jpg?oh=9a39d1f6d0fedea8eccca435b2e7b9ad&oe=56707E76"><img
+                        src="https://scontent-ams2-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/390211_298268603552686_1393108024_n.jpg?oh=9a39d1f6d0fedea8eccca435b2e7b9ad&oe=56707E76"
+                        alt="Valdur" ;></a>
+
+                <div class="d">ENTRUM sessioon</div>
             </td>
         </tr>
     </table>
@@ -33,7 +53,7 @@
     <br/>
 </div>
 <div id="footer">
-    <a href="mailto:valdur.kana@khk.ee">Küsimused saata siia</a>
+    <a href="mailto:valdur.kana@khk.ee">Kirjutage Valdurile!</a>
 </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -12,12 +12,12 @@
     <h4>Tutvustav leht</h4>
 </div>
 <div class="top">
-    <a href="gallery.php">Gallerii</a>&nbsp;&nbsp;
+    <a href="gallery.php">Galerii</a>&nbsp;&nbsp;
     <a href="https://www.facebook.com/valdur55">Valduri Facebook</a>
 </div>
 <div class="right">
     <img id="image"
-         src="img/valdur.jpg"/>
+         src="img/valdur.jpg" alt="ENTRUM Gala <br/> Vasakul Yrjo Ojasaar, paremal Valdur">
 
     <ul>
         <li>Valdur</li>
@@ -39,46 +39,335 @@
 
     <p>Nimi Valdur Kana on Eestis haruldane. Sellise nimega võib kohata kuni 4 inimest, kuid on võimalik, et
         see on ainulaadne.</p>
-    <br/>
 
+    <p>Tema lemmikraamatud on:</p>
+    <ol>
+        <li>
+            "Optimisti teekond tulevikku" - Mark Stevenson
+        </li>
+        <li>
+            “Häkkerieetika ja informatsiooniajastu vaim” - Pekka Himanen
+        </li>
+        <li>
+            "Muutuv aju" - Norman Doidge
+        </li>
+    </ol>
     <p>Valdur on käinud mitmes koolis:</p>
-    <table border="1">
+    <table id="school" border="1">
         <th>Kool</th>
-        <th>Eriala</th>
+        <th>Eriala/haridus</th>
         <th>Algusaasta</th>
         <th>Lõpuaasta</th>
         <tr>
             <td>Tartu KHK</td>
-            <td>Noorem tarkvaraarendaja (veebispetsialist)</td>
+            <td>Noorem tarkvaraarendaja (veebispetsialist)/kutseharidus</td>
             <td>2015</td>
             <td>-</td>
         </tr>
         <td>Tartu Ülikool</td>
-        <td>Informaatika</td>
+        <td>Informaatika/kõrgharidus</td>
         <td>2015</td>
         <td>2015</td>
         <tr>
             <td>Tartu Ülikool</td>
-            <td>Arvutitehnika</td>
+            <td>Arvutitehnika/kõrgharidus</td>
             <td>2013</td>
             <td>2015</td>
         </tr>
         <tr>
             <td>Tartu Tamme Gümnaasium</td>
-            <td>-</td>
+            <td>-/keskharidus</td>
             <td>2010</td>
             <td>2013</td>
         </tr>
         <tr>
             <td>Otepää Gümnaasium</td>
-            <td>-</td>
+            <td>-/põhiharidus</td>
             <td>2001</td>
             <td>2010</td>
         </tr>
     </table>
-</div>
-<div id="footer">
-    <a href="mailto:valdur.kana@khk.ee">Küsimused saata siia</a>
-</div>
+    <br/>
+    <hr/>
+    <br/>
+
+    <div class="typeracer">
+        <div style="width: 100%;">
+
+
+            <table border="1">
+                <tbody>
+                <tr>
+                    <td>
+                        <h3>Valduri typeracer'i profiil</h3>
+
+                </tr>
+                </tbody>
+            </table>
+
+            <table border="1" style="border-bottom: solid 1px gray;">
+                <tbody>
+                <tr>
+                    <td nowrap="">
+                        valdur55&nbsp;&nbsp;
+
+                    </td>
+                    <td nowrap="" align="right">Average of the last 10 races - 59 WPM&nbsp;&nbsp;</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <table border="1" cellspacing="5">
+                <tbody>
+                <tr>
+
+                    <td>Name</td>
+                    <td>
+                        Valdur Kana
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>Gender</td>
+                    <td>
+                        Male
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>Location</td>
+                    <td>
+
+
+                        Estonia
+
+                    </td>
+
+                <tr height="20"></tr>
+
+                <tr>
+
+
+                    <tbody>
+                <tr>
+                    <td title="Average of all races" nowrap="">Full Average</td>
+                    <td>
+                        58.8 WPM
+                    </td>
+                </tr>
+                <tr>
+                    <td>Best Race</td>
+                    <td>
+                        71 WPM
+                    </td>
+                </tr>
+                <tr>
+                    <td>Races Completed&nbsp;&nbsp;</td>
+                    <td>
+                        8
+                    </td>
+                </tr>
+
+                <tr height="20"></tr>
+
+                <tr>
+                    <td>WPM Percentile</td>
+                    <td>
+                        72.4%
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Skill Level</td>
+                    <td>
+                        Typemaster
+                    </td>
+                </tr>
+                <tr>
+                    <td>Experience Level</td>
+                    <td>
+
+                        Typist 0
+
+                    </td>
+                </tr>
+
+                <tr height="20"></tr>
+
+                <tr>
+                    <td>Racing Since</td>
+                    <td>
+                        Sept. 13, 2015&nbsp;&nbsp;
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>Keyboard</td>
+                    <td>
+                        Qwerty
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>Membership</td>
+                    <td>
+                        Basic
+
+                    </td>
+                </tr>
+
+
+                </tbody>
+            </table>
+            </td>
+            </tr>
+            </tbody>
+            </table>
+
+
+            <h3>Latest Race Results</h3>
+            <table border="1" class="scoresTable">
+                <tbody>
+                <tr>
+                    <th>Race #</th>
+                    <th>Speed</th>
+                    <th>Accuracy</th>
+                    <th>Place</th>
+                    <th>Date</th>
+                </tr>
+
+
+                <tr>
+
+                    <td>8</td>
+                    <td>71 WPM&nbsp;&nbsp;</td>
+                    <td>96.6%&nbsp;&nbsp;</td>
+                    <td>4/5&nbsp;&nbsp;</td>
+                    <td>
+
+
+                        Sept. 13, 2015&nbsp;&nbsp;
+
+                    </td>
+                </tr>
+
+
+                <tr>
+
+                    <td>7</td>
+                    <td>57 WPM</td>
+                    <td>94.2%</td>
+                    <td>5/5</td>
+                    <td>
+
+
+                        Sept. 13, 2015
+
+                    </td>
+                </tr>
+
+
+                <tr>
+
+                    <td>6</td>
+                    <td>60 WPM</td>
+                    <td>95.0%</td>
+                    <td>2/3</td>
+                    <td>
+
+
+                        Sept. 13, 2015
+
+                    </td>
+                </tr>
+
+
+                <tr>
+
+                    <td>5</td>
+                    <td>53 WPM</td>
+                    <td>90.6%</td>
+                    <td>4/4</td>
+                    <td>
+
+
+                        Sept. 13, 2015
+
+                    </td>
+                </tr>
+
+
+                <tr>
+
+                    <td>4</td>
+                    <td>60 WPM</td>
+                    <td>93.0%</td>
+                    <td>4/5</td>
+                    <td>
+
+
+                        Sept. 13, 2015
+
+                    </td>
+                </tr>
+
+
+                <tr>
+
+                    <td>3</td>
+                    <td>52 WPM</td>
+                    <td>89.3%</td>
+                    <td>3/3</td>
+                    <td>
+
+
+                        Sept. 13, 2015
+
+                    </td>
+                </tr>
+
+
+                <tr>
+
+                    <td>2</td>
+                    <td>57 WPM</td>
+                    <td>92.9%</td>
+                    <td>1/4</td>
+                    <td>
+
+
+                        Sept. 13, 2015
+
+                    </td>
+                </tr>
+
+
+                <tr>
+
+                    <td>1</td>
+                    <td>60 WPM</td>
+                    <td>96.4%</td>
+                    <td>3/4</td>
+                    <td>
+
+
+                        Sept. 13, 2015
+
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+
+
+        </div>
+    </div>
+    <div id="footer">
+        <a href="mailto:valdur.kana@khk.ee">Kirjutage Valdurile!</a>
+    </div>
 </body>
 </html>
